90e3ee3a3e1302357c3090664237cf71009c507e
Roman Rozinko <r.rozinko@gmail.com>
2025-05-23 11:32:04 +0300

    add tabview code for ios 18

--- DuckTracker/WrapperView.swift
+++ DuckTracker/WrapperView.swift
@@ -5,29 +5,47 @@ struct WrapperView: View {
     @State var selectedTab = 1
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            MainScreen(selectedTab: $selectedTab)
-                .tabItem {
-                    Label(".tab.home", systemImage: "flag.fill")
+        if #available(iOS 18.0, *) {
+            TabView(selection: $selectedTab) {
+                Tab(".tab.home", systemImage: "flag.fill", value: 1) {
+                    MainScreen(selectedTab: $selectedTab)
                 }
-                .tag(1)
-            ActiveTrackScreen(selectedTab: $selectedTab)
-                .tabItem {
-                    Label(".tab.track", systemImage: "play.fill")
+                Tab(".tab.track", systemImage: "play.fill", value: 2) {
+                    ActiveTrackScreen(selectedTab: $selectedTab)
                 }
-                .tag(2)
-            HistoryScreen(selectedTab: $selectedTab)
-                .tabItem {
-                    Label(".tab.history", systemImage: "calendar")
+                Tab(".tab.history", systemImage: "calendar", value: 3) {
+                    HistoryScreen(selectedTab: $selectedTab)
                 }
-                .tag(3)
-            SettingsScreen(selectedTab: $selectedTab)
-                .tabItem {
-                    Label(".tab.settings", systemImage: "gear")
+                Tab(".tab.settings", systemImage: "gear", value: 4) {
+                    SettingsScreen(selectedTab: $selectedTab)
                 }
-                .tag(4)
+            }
+            .tabViewStyle(.tabBarOnly)
+        } else {
+            TabView(selection: $selectedTab) {
+                MainScreen(selectedTab: $selectedTab)
+                    .tabItem {
+                        Label(".tab.home", systemImage: "flag.fill")
+                    }
+                    .tag(1)
+                ActiveTrackScreen(selectedTab: $selectedTab)
+                    .tabItem {
+                        Label(".tab.track", systemImage: "play.fill")
+                    }
+                    .tag(2)
+                HistoryScreen(selectedTab: $selectedTab)
+                    .tabItem {
+                        Label(".tab.history", systemImage: "calendar")
+                    }
+                    .tag(3)
+                SettingsScreen(selectedTab: $selectedTab)
+                    .tabItem {
+                        Label(".tab.settings", systemImage: "gear")
+                    }
+                    .tag(4)
+            }
+            .accentColor(Color.wrapperViewAccent)
         }
-        .accentColor(Color.wrapperViewAccent)
     }
 }
 
